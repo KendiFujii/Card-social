87e4ceb05e0bc7530f2a7507d1e9bd80b45053af
KendiFujii <117840322+KendiFujii@users.noreply.github.com>
2023-02-06 15:13:56 -0300

    Alteração da descrição da imagem

--- script.js
+++ script.js
@@ -14,3 +14,19 @@ img.setAttribute('src', './assests/avatar-light.png')
 img.setAttribute("src", "./assests/avatar.png")
   }
 }
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  //pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  //substituir a imagem
+  if (html.classList.contains('light')) {
+    //se tiver light mode, adicionar a imagem light
+img.setAttribute("alt", "Foto de icone, uma capivara.")
+  } else {
+    //set tiver sem light mode, manter a imagem normal
+img.setAttribute("alt", "Foto de icone, usado pelo Fukuisan.")
+  }
+}
