b1d258ccd27b686334fd69e8f41aec87b61e9a3f
KendiFujii <117840322+KendiFujii@users.noreply.github.com>
2023-02-06 16:14:12 -0300

    Alteração CSS

--- script.js
+++ script.js
@@ -8,25 +8,9 @@ function toggleMode() {
   //substituir a imagem
   if (html.classList.contains('light')) {
     //se tiver light mode, adicionar a imagem light
-img.setAttribute('src', './assests/avatar-light.png')
+img.setAttribute("src", "./assests/avatar-light.png")
   } else {
     //set tiver sem light mode, manter a imagem normal
 img.setAttribute("src", "./assests/avatar.png")
-  }
-}
-function toggleMode() {
-  const html = document.documentElement
-  html.classList.toggle("light")
-
-  //pegar a tag img
-  const img = document.querySelector("#profile img")
-
-  //substituir a imagem
-  if (html.classList.contains('light')) {
-    //se tiver light mode, adicionar a imagem light
-img.setAttribute("alt", "Foto de icone, uma capivara.")
-  } else {
-    //set tiver sem light mode, manter a imagem normal
-img.setAttribute("alt", "Foto de icone, usado pelo Fukuisan.")
   }
 }
